562aa3f5cf053bbc2913ea8ad778e273b80c5a15
Phanuwatphk <flook5067@gmail.com>
2022-02-06 00:55:43 +0700

    second commit

--- Box_plot.c
+++ Box_plot.c
@@ -23,19 +23,19 @@ int main(){
     }
 
     if(arrsize % 2 == 0){//even number
+        float q1 = (float) arr[(arrsize / 2) / 2];
         float q2 = (float) (arr[(arrsize / 2) - 1] + arr[(arrsize / 2)]) / 2;
-        int q1 = arr[(arrsize / 2) / 2];// error
-        int q3 = arr[(arrsize + (arrsize / 2)) / 2];// error
-        printf("minimum = %d\nmaximum = %d\nquartile_1 = %d\nquartile_2 = %.3f\nquartile_3 = %d", arr[0], arr[arrsize - 1], q1, q2, q3);
+        float q3 = (float) arr[(arrsize + ((arrsize / 2) - 1)) / 2];
+        printf("minimum = %d\nmaximum = %d\nquartile_1 = %.2f\nquartile_2 = %.2f\nquartile_3 = %.2f", arr[0], arr[arrsize - 1], q1, q2, q3);
     }
     else if(arrsize % 2 != 0){//odd number
-        float q2 = arr[arrsize / 2];
         float q1 = (float) arr[(arrsize / 2) / 2];
+        float q2 = (float) arr[arrsize / 2];
         float q3 = (float) arr[(arrsize + (arrsize / 2)) / 2];
-        printf("minimum = %d\nmaximum = %d\nquartile_1 = %.3f\nquartile_2 = %d\nquartile_3 = %.3f", arr[0], arr[arrsize - 1], q1, q2, q3);
+        printf("minimum = %d\nmaximum = %d\nquartile_1 = %.2f\nquartile_2 = %.2f\nquartile_3 = %.2f", arr[0], arr[arrsize - 1], q1, q2, q3);
     }
 
-    printf("\n...Press any key to end program...");
+    printf("\n\n...Press any key to end program...");
     getch();
     return 0;
 }
