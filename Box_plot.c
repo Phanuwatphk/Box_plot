cb671e8e858b02a5965a8f35cc9874e4cfa79784
Phanuwatphk <flook5067@gmail.com>
2022-02-26 22:21:34 +0700

    last time

--- Box_plot.c
+++ Box_plot.c
@@ -2,15 +2,16 @@
 #include <conio.h>
 
 int main(){
-    int i, j, c, arr[100], arrsize;
+    int i, j, c, arrsize;
+    float arr[100];
 
     for(i = 0;i < 100;i++){
         printf("Enter number[%d] : ", i + 1);
-        scanf("%d", &arr[i]);
-	if(arr[i] == 0){
-		arrsize = i;
-		break;
-	}
+        scanf("%f", &arr[i]);
+	    if(arr[i] == 0){
+		    arrsize = i;
+		    break;
+	    }
     }
     for(i = 0;i < arrsize;i++){
         for(j = i + 1;j < arrsize;j++){
@@ -23,16 +24,16 @@ int main(){
     }
 
     if(arrsize % 2 == 0){//even number
-        float q1 = (float) arr[(arrsize / 2) / 2];
-        float q2 = (float) (arr[(arrsize / 2) - 1] + arr[(arrsize / 2)]) / 2;
-        float q3 = (float) arr[(arrsize + ((arrsize / 2) - 1)) / 2];
-        printf("minimum = %d\nmaximum = %d\nquartile_1 = %.2f\nquartile_2 = %.2f\nquartile_3 = %.2f", arr[0], arr[arrsize - 1], q1, q2, q3);
+        float q1 = arr[(arrsize / 2) / 2];
+        float q2 = (arr[(arrsize / 2) - 1] + arr[(arrsize / 2)]) / 2;
+        float q3 = arr[(arrsize + ((arrsize / 2) - 1)) / 2];
+        printf("minimum = %.0f\nmaximum = %.0f\nquartile_1 = %.2f\nquartile_2 = %.2f\nquartile_3 = %.2f", arr[0], arr[arrsize - 1], q1, q2, q3);
     }
     else if(arrsize % 2 != 0){//odd number
-        float q1 = (float) arr[(arrsize / 2) / 2];
-        float q2 = (float) arr[arrsize / 2];
-        float q3 = (float) arr[(arrsize + (arrsize / 2)) / 2];
-        printf("minimum = %d\nmaximum = %d\nquartile_1 = %.2f\nquartile_2 = %.2f\nquartile_3 = %.2f", arr[0], arr[arrsize - 1], q1, q2, q3);
+        float q1 = arr[(arrsize / 2) / 2];
+        float q2 = arr[arrsize / 2];
+        float q3 = arr[(arrsize + (arrsize / 2)) / 2];
+        printf("minimum = %.0f\nmaximum = %.0f\nquartile_1 = %.2f\nquartile_2 = %.2f\nquartile_3 = %.2f", arr[0], arr[arrsize - 1], q1, q2, q3);
     }
 
     printf("\n\n...Press any key to end program...");
